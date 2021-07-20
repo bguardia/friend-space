dc208bfa326ceec5ae76ba7aa58c07166510688f
Blair Guardia <blairguardia@gmail.com>
2021-07-20 22:13:28 +0900

    Update Posts and Comments routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  resources :posts do
+    resources :comments, shallow: true
+  end
+  resources :comments, only: [:edit, :update, :destroy]
+
   root to: "posts#index"
 end
