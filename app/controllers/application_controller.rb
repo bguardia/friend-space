a2693bde2e23eefb29607ee03d0edfd6a9107cc7
Blair Guardia <blairguardia@gmail.com>
2021-08-09 14:47:13 +0900

    Add User panel

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,19 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user! 
     before_action :user_has_completed_profile
+    #
+    # helper :profile_picture_for
+
+    def profile_picture_url_for(user, options)
+        w = options.fetch(:width, nil) || options.fetch(:height, nil) || 64
+        h = options.fetch(:height, nil) || w
+
+        if user.profile.avatar.attached?
+            url_for user.profile.avatar.variant(resize_to_fit: [w, h])
+        else
+            "https://via.placeholder.com/#{w}x#{h}"
+        end
+    end
 
     private 
     def user_has_completed_profile
@@ -8,4 +21,6 @@ class ApplicationController < ActionController::Base
             redirect_to new_profile_path unless current_user.profile
         end
     end
+
+
 end
