21962a6b33a9bd0dd995fdce21c0f82e7c14f74d
Blair Guardia <blairguardia@gmail.com>
2021-08-11 10:34:46 +0900

    Add User Search Bar to Friends Index
    Now users can search among their list of friends on the Friends index
    page. The friends count will show how many results matched for a given
    query, adding a short string that clarifies the list has been filtered.

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,6 +1,15 @@
 class FriendshipsController < ApplicationController
 
     def index
-        @friends = current_user.friends.all
+        if params.key?(:username)
+            name_query = "%#{params.fetch(:username, "")}%"
+            @friends = User.includes(:friend_requests, :friends)
+                         .joins(profile: [:avatar_blob])
+                         .where(id: current_user.friends.ids)
+                         .where("CONCAT(profiles.firstname, ' ', profiles.lastname) ILIKE ?", name_query)
+                         .all
+        else
+            @friends = current_user.friends.all
+        end
     end
 end
