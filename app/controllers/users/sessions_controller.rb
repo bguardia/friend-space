92218806d200546fcfd937de7caf06f96efb5341
Blair Guardia <blairguardia@gmail.com>
2021-09-05 13:36:44 +0900

    Update User Profile Checking
    Set devise registrations and sessions controllers in routes.rb.
    Registrations controller now sends newly registered users to the
    profile#new page instead of relying on the before_action check of the
    user's profile.
    
    Sessions controller now skips this same before action, preventing an
    error where a user who hadn't completed their profile can't sign out.

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,7 +2,8 @@
 
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-
+  skip_before_action :user_has_completed_profile
+  
   # GET /resource/sign_in
   # def new
   #   super
