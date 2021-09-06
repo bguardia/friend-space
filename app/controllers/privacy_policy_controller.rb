831bdf2f9ff37f323aeb662e791e89430426b8d3
Blair Guardia <blairguardia@gmail.com>
2021-09-06 15:59:08 +0900

    Remove User Profile Check filter

--- app/controllers/privacy_policy_controller.rb
+++ app/controllers/privacy_policy_controller.rb
@@ -1,5 +1,5 @@
 class PrivacyPolicyController < ApplicationController
-    skip_before_action :authenticate_user!, :user_has_completed_profile
+    skip_before_action :authenticate_user!
 
     def show
     end
