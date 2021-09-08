316bc7dbd71cdbbbfcb9d51118c5622fb23ac7b6
Blair Guardia <blairguardia@gmail.com>
2021-09-08 12:16:08 +0900

    Add destroy dependencies to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,19 +5,19 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   devise :omniauthable, omniauth_providers: %i[facebook]
   
-  has_one :profile
-  has_many :posts
-  has_many :comments
-  has_many :likes
+  has_one :profile, dependent: :destroy
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
   
-  has_many :notifications, foreign_key: "receiver_id"
-  has_many :friend_requests, foreign_key: "receiver_id"
-  has_many :sent_friend_requests, class_name: "FriendRequest", foreign_key: "sender_id"
+  has_many :notifications, foreign_key: "receiver_id", dependent: :destroy
+  has_many :friend_requests, foreign_key: "receiver_id", dependent: :destroy
+  has_many :sent_friend_requests, class_name: "FriendRequest", foreign_key: "sender_id", dependent: :destroy
   scope :sent_request_to, ->(user) { joins("INNER JOIN friend_requests ON users.id = friend_requests.sender_id").where(friend_requests: {receiver_id: user.id})}
   scope :received_request_from, ->(user) { joins("INNER JOIN friend_requests ON users.id = friend_requests.receiver_id").where(friend_requests: {sender_id: user.id})}
   scope :not_pending_with, ->(user) { where.not(id: sent_request_to(user).ids.concat(received_request_from(user).ids))}
 
-  has_many :friendships
+  has_many :friendships, dependent: :destroy
   has_many :friends, through: :friendships
   scope :not_friends_with, ->(user) { where.not(id: joins(:friendships).select(:friend_id).where(id: user.id)).where.not(id: user.id)}
 
