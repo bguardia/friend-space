073f58f328cbdf5878f54e2729a53e1fad3e88c2
Blair Guardia <blairguardia@gmail.com>
2022-08-09 18:33:45 +0900

    Add profile_picture_url helper
    profile_picture_url helper takes a user parameter and a dimensions array
    parameter and returns the url to the user's profile photo in the
    requested size if it exists.
    
    If there is no profile picture or the attached avatar is not an image,
    it returns the url to a placeholder image.
    
    The helper has replaced several instances of redundant logic in
    user-related views and partials.

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,2 +1,6 @@
 module ProfilesHelper
+    def profile_picture_url(user, dimension_array = [64, 64])
+        @avatar = user.profile.avatar.attached? && user.profile.avatar.image? ? user.profile.avatar : nil
+        @avatar ? url_for(@avatar.variant(resize_to_fill: dimension_array)) : "https://via.placeholder.com/#{dimension_array[0]}x#{dimension_array[1]}"
+    end
 end
